64f04845bead4e6bf95357a4236e6bbcfc7ee103
alnor <alxrsk@mc.loc>
2022-08-09 11:39:43 +0300

    init

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import appStyles from './app.module.css';
+
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div className={appStyles.app}>
+      </div>
+    );
+  }
+}
+
+export default App;
