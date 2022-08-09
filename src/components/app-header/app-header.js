0bdde4ff6d669dded4c3ba306196cf615ac94c17
alnor <alxrsk@mc.loc>
2022-08-09 22:02:21 +0300

    верстка navbar

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import headerStyles from './app-header.module.css';
-import { Logo, Button, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import logoPath from '../../images/logo.svg'
+import { Button, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
 class AppHeader extends React.Component {
   render() {
     return (
-      <nav className={headerStyles.nav}>
-        <div className='{headerStyles.leftBox}'>
-          <Button type="secondary" size="medium"><BurgerIcon type="primary" /><span className="text text_type_main-default">Конструктор</span></Button>
-          <Button type="secondary" size="medium"><ListIcon type="primary" /><span className="text text_type_main-default">Лента заказов</span></Button>
+      <div className={headerStyles.nav} style={{backgroundImage: `url(${logoPath})`}}>
+        <Button type="secondary" size="medium" ><BurgerIcon type="primary" /><span className="text text_type_main-default">Конструктор</span></Button>
+        <Button type="secondary" size="medium" ><ListIcon type="primary" /><span className="text text_type_main-default">Лента заказов</span></Button>
+        <div className={headerStyles.rightItems}>
+          <Button type="secondary" size="medium" ><ProfileIcon type="primary" /><span className="text text_type_main-default">Личный кабинет</span></Button>
         </div>
-        <div className='{headerStyles.navBox}'>
-          <Logo className={headerStyles.logo} />
-        </div>
-        <div className='{headerStyles.rightBox}'>
-          <Button type="secondary" size="medium"><ProfileIcon type="primary" /><span className="text text_type_main-default">Личный кабинет</span></Button>
-        </div>
-      </nav>
+      </div>
     );
   }
 }
