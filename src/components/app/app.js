9185257147198624d873024021e50a8461bbc80d
AlekseyOreshkin <110759553+AlekseyOreshkin@users.noreply.github.com>
2022-08-14 20:10:05 +0300

    Revert "Sprint 1/step 2"

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,38 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import appStyles from './app.module.css';
-import AppHeader from '../app-header/app-header'
-import BurgerIngredients from '../burger-ingredients/burger-ingredients'
-import BurgerConstructor from '../burger-constructor/burger-constructor';
-import { IngredientsUrl } from '../../utils/constants';
 
-const App = () => {
 
-  const [state, setState] = useState({ingredients: {success: false, data: []} });
-  
-  useEffect(() => {
-    console.log('Загрузка ингредиентов');
-    fetch(IngredientsUrl)
-      .then(response => response.json())
-      .then(({success, data}) => {
-        if (success) {
-          console.log(`Загружено ингредиентов: ${data.length}`);
-          setState({ ...state, ingredients: {success: success, data: data} });
-        } else {
-          console.log('Неизвестный ответ сервера. Пока без обработки');
-        }
-      }).catch(error => {
-        console.log(`Ошибка выполнения запроса: ${error}`);
-      });
+class App extends React.Component {
+  render() {
 
-  }, []);
-  return (
-    <div className='main-grid' >
-      <AppHeader />
-      <h1 className={`text_type_main-large ${appStyles.staticText}`}>Соберите бургер</h1>
-      <BurgerIngredients data = {state.ingredients.data}/>
-      <BurgerConstructor data = {state.ingredients.data}/>
-    </div>
-  );
+    return (
+      <div className={appStyles.app}>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-
-const IngredientsUrl = 'https://norma.nomoreparties.space/api/ingredients';
-
-export {
-    IngredientsUrl
-};
