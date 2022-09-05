9b9a925329f4c1a05b4dfdd3bb6b4091953ab311
Aleksey Oreshkin <oreshkinan@taxcom.ru>
2022-09-05 16:31:34 +0300

    endpoints

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,14 @@ export const INGREDIENTS_ENDPOINT = '/ingredients';
 export const ORDER_REQUEST_ENDPOINT = '/orders';
 export const PASSWORD_RESET_ENDPOINT = '/password-reset';
 export const PASSWORD_SET_ENDPOINT = '/password-reset/reset';
+export const AUTH_LOGIN_ENDPOINT = '/auth/login'; // эндпоинт для авторизации.
+export const AUTH_REGISTER_ENDPOINT = '/auth/register'; // эндпоинт для регистрации пользователя.
+export const AUTH_LOGOUT_ENDPOINT = '/auth/logout'; // эндпоинт для выхода из системы.
+export const AUTH_REFRESH_TOKEN_ENDPOINT = '/auth/token'; // эндпоинт обновления токена.
+
 
 export const RESET_PASSWORD_STEP_RECOVER = 'RESET_PASSWORD_STEP_RECOVER';
 export const RESET_PASSWORD_STEP_RESET   = 'RESET_PASSWORD_STEP_RESET';
 export const RESET_PASSWORD_STEP_LOGIN = 'RESET_PASSWORD_STEP_LOGIN';
+
 
