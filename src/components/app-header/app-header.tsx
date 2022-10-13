edf0161606d86a9231406a240bbc1e0b271c4838
AlekseyOreshkin <alxrsk@yandex.ru>
2022-10-13 21:13:06 +0300

    Link to home

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,17 +1,23 @@
 import { FC } from 'react';
 import styles from './app-header.module.css';
-import logoPath from '../../images/logo.svg'
 import { AppHeaderButton } from '../app-header-button/app-header-button';
+import { Link, useLocation } from 'react-router-dom';
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 
 const ROOT = '/';
 const FEED = '/feed';
 const LK = '/profile';
 
 export const AppHeader: FC = () => {
+  const { pathname } = useLocation();
+
   return (
-    <header className={styles.nav} style={{ backgroundImage: `url(${logoPath})` }}>
-      <AppHeaderButton path={ROOT} icon='burger' text='Конструктор' />
-      <AppHeaderButton path={FEED} icon='list' text='Лента заказов' />
+    <header className={styles.nav} >
+      <div className={styles.leftItems}>
+        <AppHeaderButton path={ROOT} icon='burger' text='Конструктор' />
+        <AppHeaderButton path={FEED} icon='list' text='Лента заказов' />
+      </div>
+      <Link className={styles.homeLink} to={{ pathname: '/', state: { from: pathname } }}><Logo/></Link>
       <div className={styles.rightItems}>
         <AppHeaderButton path={LK} icon='profile' text='Личный кабинет' />
       </div>
